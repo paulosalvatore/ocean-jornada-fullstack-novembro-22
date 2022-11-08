40a96ee4272409ef62428bf014fae3883c0020b8
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-08 20:08:22 -0300

    Endpoint Read All implementado

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
 
+// Lista de informações
+const itens = ["Café Pelé", "Café Pilão", "Café Arábico"];
+//             0             1             2
+
+// Endpoint [GET] /itens READ ALL (Ler todos os itens)
+app.get("/itens", function (req, res) {
+  res.send(itens);
+});
+
 app.listen(3000, function () {
   console.log("Servidor rodando em http://localhost:3000");
 });
