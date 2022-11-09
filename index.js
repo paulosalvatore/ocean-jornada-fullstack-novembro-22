baba24c277eb17da0ece552be2a6997c00c9756c
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-08 21:23:57 -0300

    Endpoint CREATE `/itens` implementado

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Sinalizamos que estamos usando JSON no Body
+app.use(express.json());
+
 // Endpoint principal
 app.get("/", function (req, res) {
   res.send("Hello World!!!");
@@ -15,9 +18,22 @@ app.get("/oi", function (req, res) {
 const itens = ["Café Pelé", "Café Pilão", "Café Arábico"];
 //             0             1             2
 
-// Endpoint [GET] /itens READ ALL (Ler todos os itens)
+// Endpoint [GET] /itens - READ ALL (Ler todos os itens)
 app.get("/itens", function (req, res) {
   res.send(itens);
+});
+
+// Endpoint [POST] /itens - CREATE (Criar um item)
+app.post("/itens", function (req, res) {
+  // console.log(req.body);
+
+  // Pegamos o nome enviado no body
+  const item = req.body.nome;
+
+  // Inserimos o valor recebido na lista
+  itens.push(item);
+
+  res.send("Item criado com sucesso!");
 });
 
 app.listen(3000, function () {
