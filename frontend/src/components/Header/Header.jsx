11f47cb1ef70df2ad31b17866ca6cb1a8d278847
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-21 21:47:34 -0300

    Finalização do `Header`

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import "./Header.css"
+import Brand from "../../assets/brand.svg"
+
 function Header() {
-    return <div>Header</div>
+  return (
+    <div className="Header">
+      <a href="/">
+        <img src={Brand} width={32} height={32} alt="Samsung Ocean Logo" />
+      </a>
+
+      <a href="/">Listar</a>
+
+      <a href="/adicionar">Adicionar</a>
+    </div>
+  )
 }
 
 export default Header;
