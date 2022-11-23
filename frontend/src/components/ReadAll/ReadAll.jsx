11d0551c9168c5aaa0d0456b8a858b7ebb712e9f
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-22 21:55:28 -0300

    `ReadAll` lendo componentes a partir da lista

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -2,18 +2,25 @@ import ItemCard from "../ItemCard/ItemCard";
 import "./ReadAll.css"
 
 function ReadAll() {
+  const itens = [
+    {
+      _id: "1234", 
+      nome: "Item 1", 
+      imagemUrl: "https://picsum.photos/201/201" 
+    },
+    { 
+      _id: "9999", 
+      nome: "Item 2", 
+      imagemUrl: "https://picsum.photos/200/200" 
+    }
+  ];
+  console.log(itens);
+
   return (
     <div className="ReadAll">
-      <ItemCard item={{ 
-        _id: "1234", 
-        nome: "Item 1", 
-        imagemUrl: "https://picsum.photos/201/201" 
-      }} />
-      <ItemCard item={{ 
-        _id: "9999", 
-        nome: "Item 2", 
-        imagemUrl: "https://picsum.photos/200/200" 
-      }} />
+      {itens.map(function (item, index) {
+        return <ItemCard key={`item-card-${index}`} item={item} />
+      })}
     </div>
   )
 }
