93f36a99f71690357b3d9c01c47919273ce92cf0
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-24 20:22:04 -0300

    Feedback de sucesso/erro no `Create`

--- frontend/src/components/Create/Create.jsx
+++ frontend/src/components/Create/Create.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { Api } from "../../api/api";
 import "./Create.css"
 
 function Create() {
+  const navigate = useNavigate();
+
   async function processarSubmit(event) {
     event.preventDefault();
 
@@ -13,13 +16,16 @@ function Create() {
       imagemUrl
     };
 
-    console.log(payload);
-
     const createUrl = Api.itens.create();
     const response = await Api.buildApiPostRequest(createUrl, payload);
     const body = await response.json();
 
-    console.log(body);
+    if (response.status === 200) {
+      alert(body.message);
+      navigate("/");
+    } else {
+      alert("Algum erro ocorreu, tente novamente.");
+    }
   }
 
   return (
