0f3cb425bf74a8d4a30c70c6296626a68230ea6c
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-23 21:34:46 -0300

    Correção na `baseUrl` da nuvem

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 export const Api = {
   baseUrl: "http://localhost:3000/",
-  // baseUrl: "https://ocean-jornada-fullstack-novembro-22-cfwy.onrender.com",
+  // baseUrl: "https://ocean-jornada-fullstack-novembro-22-cfwy.onrender.com/",
 
   // Endpoints de itens
   itens: {
