6d4b95758b2497dc1959fe9ec176a158b34411d7
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-24 20:08:30 -0300

    Implementação do `Create` (WIP)

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -10,6 +10,9 @@ export const Api = {
     readAll: function () {
       return this.endpoint() + "/";
     },
+    create: function () {
+      return this.endpoint() + "/";
+    },
   },
 
   // Montar as requisições
@@ -17,6 +20,17 @@ export const Api = {
   buildApiGetRequest: function (url) {
     return fetch(url, {
       method: "GET",
+    });
+  },
+
+  // POST
+  buildApiPostRequest: function (url, body) {
+    return fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: new Headers({
+        "Content-type": "application/json",
+      }),
     });
   },
 };
--- frontend/src/components/Create/Create.jsx
+++ frontend/src/components/Create/Create.jsx
@@ -1,7 +1,46 @@
+import { Api } from "../../api/api";
 import "./Create.css"
 
 function Create() {
-  return <div>Create</div>
+  async function processarSubmit(event) {
+    event.preventDefault();
+
+    const nome = event.target.nome.value;
+    const imagemUrl = event.target.imagemUrl.value;
+
+    const payload = {
+      nome,
+      imagemUrl
+    };
+
+    console.log(payload);
+
+    const createUrl = Api.itens.create();
+    const response = await Api.buildApiPostRequest(createUrl, payload);
+    const body = await response.json();
+
+    console.log(body);
+  }
+
+  return (
+    <div className="Create">
+      <form onSubmit={processarSubmit}>
+        <div>
+          <label htmlFor="nome">Nome:</label>
+          <input type="text" id="nome" />
+        </div>
+
+        <div>
+          <label htmlFor="imagemUrl">URL da Imagem:</label>
+          <input type="text" id="imagemUrl" />
+        </div>
+
+        <div>
+          <input type="submit" value="Adicionar" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default Create;
