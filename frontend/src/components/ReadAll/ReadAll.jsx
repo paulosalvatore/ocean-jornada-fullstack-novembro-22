b8629cc18e5543006239c7c6d5fd41767d2a0023
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-23 21:34:37 -0300

    Implementação do `ReadAll` chamando API

--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,19 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Api } from "../../api/api";
 import ItemCard from "../ItemCard/ItemCard";
 import "./ReadAll.css"
 
 function ReadAll() {
-  const itens = [
-    {
-      _id: "1234", 
-      nome: "Item 1", 
-      imagemUrl: "https://picsum.photos/201/201" 
-    },
-    { 
-      _id: "9999", 
-      nome: "Item 2", 
-      imagemUrl: "https://picsum.photos/200/200" 
-    }
-  ];
+  const [itens, setItens] = useState();
+
+  async function realizarRequisicao() {
+    const readAllUrl = Api.itens.readAll();
+
+    const response = await Api.buildApiGetRequest(readAllUrl);
+
+    const resultado = await response.json();
+
+    setItens(resultado);
+  }
+
+  useEffect(function () {
+    realizarRequisicao();
+  }, []);
+
+  if (itens === undefined) {
+    return <div>Carregando...</div>;
+  }
 
   return (
     <div className="ReadAll">
