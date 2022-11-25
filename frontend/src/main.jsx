781b3c5304e265890e53dc70e13e8d099ae83986
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-24 21:58:09 -0300

    Revert "`BrowserRouter` substituído por `HashRouter`"
    This reverts commit 57dd1f3898ed5d0623ff2c5cf5bbb6a315952343.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
